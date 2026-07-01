fbc3241ebe4cb6eb61236bd253281edc25417baa
arre-gu <arash.darakhsh@ait.gu.se>
2026-07-01 15:17:51 +0200

    renamed encoder to extractor. a tensor passes through torch-summary

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from params import Params
 from torch.nn import Module
 from torchsummary import summary
 from utils.gradient_reversal import ReverseLayerF
-from utils.layer_creation import make_layers
+from utils.layer_creation import create_mlp
 
 
 class DANN(Module):
@@ -13,49 +13,64 @@ class DANN(Module):
     def __init__(self, p: Params):
         super().__init__()
 
-        self.encoder = Encoder()
+        self.extractor = Extractor(p.get_vars_from_prefix("extractor", strip_prefix=True))
 
-        self.classifier = Classifier()
+        self.classifier = Classifier(p.get_vars_from_prefix("classifier", strip_prefix=True))
 
         self.gradient_reversal = ReverseLayerF()
 
-        self.discriminator = Discriminator()
+        self.discriminator = Discriminator(p.get_vars_from_prefix("discriminator", strip_prefix=True))
     
     def forward(self, x):
-        pass
+        x = x # batch we get from dataloader [B,1,D]
+        x_feature = self.extractor(x)
+        x_classifier = self.classifier(x_feature)
+        x_discriminator = self.discriminator(x_feature)
+
+        return x_classifier, x_discriminator
+        
 
     def predict(self, x):
         pass
 
 
-class Encoder(Module):
+class Extractor(Module):
 
-    def __init__(self):
+    def __init__(self, config: dict):
         super().__init__()
+        self.config = config
+        self.extractor = create_mlp(config)
     
     def forward(self, x):
-        pass
+        x = self.extractor(x)
+        return x
 
 class Classifier(Module):
 
-    def __init__(self):
+    def __init__(self, config: dict):
         super().__init__()
+        self.config = config
+        self.classifier = create_mlp(config)
     
     def forward(self, x):
-        pass
+        x = self.classifier(x)
+        return x
 
 class Discriminator(Module):
 
-    def __init__(self):
+    def __init__(self, config: dict):
         super().__init__()
+        self.config = config
+        self.discriminator = create_mlp(config)
     
     def forward(self, x):
-        pass
+        x = self.discriminator(x)
+        return x
 
 
 if __name__ == "__main__":
     
     p = Params()
     dann = DANN(p)
-    example_input = torch.randn((1,28,28))
-    summary(dann, example_input)
+    example_input = torch.randn((5,1,10))
+    summary(dann, example_input, batch_dim=None)
