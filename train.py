8cc0f2668d699f5397a412feb51fab02650574f4
arre-gu <arash.darakhsh@ait.gu.se>
2026-07-01 15:18:46 +0200

    further additional features and LOC to the train function and train script

--- train.py
+++ train.py
@@ -1,9 +1,12 @@
+import torch
 from time import time
 from tqdm import tqdm
 from model import DANN
 from params import Params
 from torch.utils.data import DataLoader
+from utils.early_stopping import EarlyStopping
 from evaluate import eval
+
 
 def train(
     model: DANN,
@@ -11,6 +14,9 @@ def train(
     train_loader: DataLoader,
     val_loader: DataLoader
     ):
+
+    stopping_criterion = EarlyStopping(p.get_vars_from_prefix("early_stopping"))
+    device = torch.device(p.device)
 
 
     print(f"Started training with device: {p.device}")
@@ -20,14 +26,15 @@ def train(
         # Train pass
         model.train()
         for batch_idx, (x,y) in enumerate(train_loader):
-            pass
+            x = x.to(device)
+            y = y.to(device)
 
         # Validation step
         val_metrics = eval(model, val_loader)
 
         # Check early stopping
-        if p.stopping_criterion(val_metrics[p.p_optim_metric]):
-            p.stopping_criterion.load_best_model(model)
+        if stopping_criterion(model, val_metrics[p.optim_metric]):
+            stopping_criterion.load_best_model(model)
             break
 
         # LR scheduler etc. 
@@ -37,4 +44,7 @@ def train(
     t_tot = (time() - t_start) / 60.0
     print(f"Finished training after {t_tot:.1f} minutes at epoch {1+epoch}")
 
+
+def eval():
+    pass
 
--- train_script.py
+++ train_script.py
@@ -1,22 +1,34 @@
 import mlflow
-from train import train
+from train import train, eval
+from params import Params
 from alc_data import ALCData
 from model import DANN
 
 def main():
 
+    p = Params()
+
     # Load data
     data = ALCData()
 
     # DataLoaders
+    train_loader = 1
+    val_loader = 1
+    test_loader = 1
 
     # Load model
     model = DANN()
 
     # Start training
-    train()
+    train(
+        model=model,
+        p=p,
+        train_loader=train_loader,
+        val_loader=val_loader
+    )
 
     # Run evaluation
+    test_metrics = eval()
 
     # Save model & optimizer
     
