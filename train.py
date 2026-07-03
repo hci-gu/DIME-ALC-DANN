85b7f854d8a078bbe4208fa7716640f8acb2d50a
arre-gu <arash.darakhsh@ait.gu.se>
2026-07-03 11:23:46 +0200

    added a lot of mlflow logging, refactored a lot of functions, fixed tons of runtime bugs

--- train.py
+++ train.py
@@ -1,64 +1,112 @@
 import torch
 import mlflow
+import torch.nn as nn
+
 from time import time
 from tqdm import tqdm
 from model import DANN
+from torch import optim
 from params import Params
 from torch.utils.data import DataLoader
 from utils.early_stopping import EarlyStopping
-from evaluate import eval
+from utils.compute_params import alpha_schedule
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 
 def train(
     model: DANN,
     p: Params,
+    optimizer: optim,
+    loss_functions: tuple[nn.BCEWithLogitsLoss, nn.CrossEntropyLoss],
     train_loader: DataLoader,
     val_loader: DataLoader
     ):
 
     stopping_criterion = EarlyStopping(p.get_vars_from_prefix("early_stopping"))
+    scheduler = ReduceLROnPlateau(optimizer=optimizer, **p.get_vars_from_prefix("scheduler"))
     device = torch.device(p.device)
+    classifier_loss_fn, discriminator_loss_fn = loss_functions # Unpack loss functions
 
 
     print(f"Started training with device: {p.device}")
     t_start = time()
-    for epoch in range(p.n_epochs):
+    for epoch in tqdm(range(p.n_epochs)):
+
+        alpha = alpha_schedule(epoch, p.n_epochs)
+        mlflow.log_metric("alpha",alpha, step=epoch)
 
         # Train pass
         model.train()
-        for batch_idx, (x,y) in enumerate(train_loader):
+        for batch_idx, (x,y,s) in enumerate(train_loader):
+            t_batch_start = time()
             x = x.to(device)
-            y = y.to(device)
+            y = y.to(device, dtype=torch.float32) # class label (intoxicated vs sober)
+            s = s.to(device, dtype=torch.long) # speaker ID 
 
-            loss = 1
+            class_logits, speaker_logits = model(x, alpha=alpha)
 
-            mlflow.log_metric("train_batch_loss", loss)
+            classifier_loss = classifier_loss_fn(class_logits.squeeze(-1), y)
+            discriminator_loss = discriminator_loss_fn(speaker_logits, s)
+
+            loss = classifier_loss + discriminator_loss
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            t_batch_time = time() - t_batch_start
+
+            global_step = epoch * len(train_loader) + batch_idx
+            mlflow.log_metrics({
+                "batch_time": t_batch_time,
+                "train_batch_classifier_loss": classifier_loss.item(),
+                "train_batch_discriminator_loss": discriminator_loss.item(),
+                "train_batch_loss": loss.item()
+            }, step=global_step
+            )
 
         # Validation step
-        val_metrics = eval(model, val_loader)
+        val_metrics = evaluate(model, p, loss_functions, val_loader, device)
 
         # Log metrics
-        mlflow.log_metrics(val_metrics)
+        mlflow.log_metrics(val_metrics, step=epoch)
+
+        # LR Scheduler
+        scheduler.step(val_metrics[p.optim_metric])
 
         # Check early stopping
         if stopping_criterion(model, val_metrics[p.optim_metric]):
-            stopping_criterion.load_best_model(model)
             break
-
-        # LR scheduler etc. 
-
     
 
     t_tot = (time() - t_start) / 60.0
     print(f"Finished training after {t_tot:.1f} minutes at epoch {1+epoch}")
+    mlflow.log_metric("training_time", t_tot)
 
 
 @torch.no_grad()
-def eval(model: DANN, eval_loader: DataLoader, device) -> dict:
+def evaluate(model: DANN, p:Params, loss_functions, eval_loader: DataLoader, device) -> dict:
     model.eval()
 
-    for batch_idx, (x,y) in enumerate(eval_loader):
-        x = x.to(device)
-        y = y.to(device)
+    classifier_loss_fn, discriminator_loss_fn = loss_functions
 
-        loss = 1
+    total_classifier_loss = 0.0
+    total_discriminator_loss = 0.0
+    for (x,y,s) in eval_loader:
+        x = x.to(device)
+        y = y.to(device, dtype=torch.float32) # class label (intoxicated vs sober)
+        s = s.to(device, dtype=torch.long) # speaker ID 
+
+        class_logits, speaker_logits = model(x, alpha=1.0)
+
+        total_classifier_loss += classifier_loss_fn(class_logits.squeeze(-1), y)
+        total_discriminator_loss += discriminator_loss_fn(speaker_logits, s)
+
+    total_classifier_loss = total_classifier_loss / len(eval_loader)
+    total_discriminator_loss = total_discriminator_loss / len(eval_loader)
+
+    return {
+        "classifier_loss": total_classifier_loss.item(),
+        "discriminator_loss": total_discriminator_loss.item(),
+        "loss": (total_classifier_loss + total_discriminator_loss).item()
+    }
+
--- train_script.py
+++ train_script.py
@@ -1,28 +1,39 @@
 import torch
 import mlflow
-from train import train, eval
+import torch.nn as nn
+
+from model import DANN
+from torch import optim
 from params import Params
 from alc_data import ALCData
-from model import DANN
+from dataclasses import asdict
+from train import train, evaluate
 from torch.utils.data import random_split, DataLoader
 
 def main():
 
+    # User parameters
     dev_run = True
     save_model = True
+    max_samples = 500 if dev_run else None
+    SEED = 1999
 
     # Mlflow tracking
     mlflow.set_experiment("DANN")
     print(f"Using MLflow Tracking URI: {mlflow.get_tracking_uri()}")
 
+    # Parameters
     p = Params(dev_run=dev_run)
     device = torch.device(p.device)
 
-    exit(0)
-
     # Load data
-    data = ALCData()
-    train_data, val_data, test_data = random_split(data, [0.8,0.1,0.1])
+    print(f"Loading data...")
+    data = ALCData(max_samples=max_samples)
+    generator = torch.Generator().manual_seed(SEED)
+    train_data, val_data, test_data = random_split(data, [0.8,0.1,0.1], generator=generator)
+    pos_weight = data.calculate_pos_weight(train_data.indices).to(device) if p.use_pos_weight else None
+
+    p.discriminator_output_dimension = len(data.speaker_id_to_index) # n_speakers
 
     # DataLoaders
     train_loader = DataLoader(train_data, p.batch_size, shuffle=True, num_workers=p.n_workers, pin_memory=p.pin_memory)
@@ -30,22 +41,34 @@ def main():
     test_loader = DataLoader(test_data, p.batch_size, shuffle=False, num_workers=p.n_workers, pin_memory=p.pin_memory)
 
     # Load model
-    model = DANN()
+    model = DANN(p)
+    model.to(device)
+
+    # Optimizer
+    optimizer_cls = getattr(torch.optim, p.optimizer)
+    optimizer = optimizer_cls(model.parameters(), **p.get_vars_from_prefix("optimizer"))
+
+    # Loss function
+    classifier_loss_fn = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
+    discriminator_loss_fn = nn.CrossEntropyLoss()
+    loss_functions = (classifier_loss_fn, discriminator_loss_fn)
 
     with mlflow.start_run():
 
-        mlflow.log_params(p)
+        mlflow.log_params(asdict(p))
 
         # Start training
         train(
             model=model,
             p=p,
+            optimizer=optimizer,
+            loss_functions=loss_functions,
             train_loader=train_loader,
             val_loader=val_loader
         )
 
         # Run evaluation
-        test_metrics = eval(model, test_loader, device)
+        test_metrics = evaluate(model, p, loss_functions, test_loader, device)
         mlflow.log_metrics(test_metrics)
 
         # Save model & optimizer
