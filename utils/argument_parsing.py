45c80e5edad4201ae834eda5c12007e8cd0cf7c4
arre-gu <arash.darakhsh@ait.gu.se>
2026-07-07 12:22:42 +0200

    some more argparse variables to control

--- utils/argument_parsing.py
+++ utils/argument_parsing.py
@@ -17,9 +17,12 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument("--dev-run", action="store_true", default=None)
     parser.add_argument("--verbose", action="store_true", default=False)
     parser.add_argument("--save-model", action="store_true", default=True)
+    parser.add_argument("--use-pos-weight", action="store_true", default=True)
     parser.add_argument("--cache-features", action="store_true", default=True)
     parser.add_argument("--batch-size", type=positive_int, default=None)
+    parser.add_argument("--seed", type=int, default=1999)
     parser.add_argument("--n-workers", "--num-workers", dest="n_workers", type=non_negative_int, default=None)
+    parser.add_argument("--n-epochs", "--num-epochs", dest="n_epochs", type=positive_int, default=None)
     parser.add_argument("--max-samples", type=positive_int, default=None)
 
     return parser.parse_args()
