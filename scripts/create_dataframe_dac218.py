e7d73348a7ef5350c3e4286ee2b613a865e54f27
arre-gu <arash.darakhsh@ait.gu.se>
2026-08-26 10:36:09 +0200

    preprocessing and dataframe creation script done

--- scripts/create_dataframe_dac218.py
+++ scripts/create_dataframe_dac218.py
@@ -8,135 +8,96 @@ from tqdm import tqdm
 from pathlib import Path
 from dataclasses import dataclass, asdict
 
-# Path variables
-ROOT_DIR = osp.join("..","data","ALC")
-WAV_H_DIR = osp.join(ROOT_DIR,"wav","h")
-LABEL_H_DIR = osp.join(ROOT_DIR,"labels","h")
+dry_run = False
 
+# Path variables
+ROOT_DIR = osp.join("..","data","SE-DAC218")
+METADATA_DIR = osp.join(ROOT_DIR,"metadata")
+WAV_DIR = osp.join(ROOT_DIR,"audio","wav")
+LABEL_DIR = osp.join(ROOT_DIR,"labels")
+
+df_sessions = pd.read_csv(osp.join(METADATA_DIR,"sessions.csv"))
+df_speakers = pd.read_csv(osp.join(METADATA_DIR,"speakers.csv"))
 
 # Feature container dataclass
 @dataclass
 class DataObject():
     id: str
     wav_file: Path
-    label_file: Path
     speaker_id: str
     session_id: str
-    prompt_id: str
-    block_id: str
-    version: int 
-    class_label: str
     age: int
     sex: str
-    alcohol_habit: str
-    br_ac: float
-    bl_ac: float
+    class_label: str # na / a
+    condition: str # A / B / C / D
+    num_sessions: int
+    session_utterances: int
+    total_utterances: int
     sound_duration: float
-    car_id: str
-    is_control_group: bool
-    channel: str = "h"
+    bac_before: float
+    bac_after: float
+    bac_delta: float
 
 
 data_dict: dict[str, DataObject] = {}
 
 # Load files
-wav_files = sorted(os.listdir(WAV_H_DIR))
-label_files = sorted(os.listdir(LABEL_H_DIR))
+wav_files = sorted(os.listdir(WAV_DIR))
 
-# Sanity check
-wave_filenames = set([Path(x).stem for x in wav_files])
-label_filenames = set([Path(x).stem[:-6] for x in label_files])
-assert wave_filenames == label_filenames, "The sets of filenames "
+with open(osp.join(LABEL_DIR,"labels.json")) as file:
+    labels = json.load(file)
+labels = {x["id"]: x["label"] for x in labels}
 
-label_files_by_audio_stem = {
-    Path(label_file).stem[:-6]: label_file
-    for label_file in label_files
-}
+assert all([(Path(wav_file).stem[:-8] in labels) for wav_file in wav_files]), f""
 
 # Loop though data
-for audio_file in tqdm(wav_files): # example file: 0061006001_h_00.wav
+for wav_file in tqdm(wav_files): # example file: spk674-sess01a-utt0001
 
-    label_file = label_files_by_audio_stem[Path(audio_file).stem]
-    underscore_idx = audio_file.index("_")
-    id = audio_file[:underscore_idx]
+    id = Path(wav_file).stem[:-8]
+    label = labels[id]
 
-    # Load label config
-    with open(osp.join(LABEL_H_DIR,label_file), 'r', encoding='utf-8') as file:
-        label_config = json.load(file)
-        label = label_config["levels"][0]["items"][0]["labels"]
-    
+    session_row = df_sessions.loc[df_sessions["session_id"] == id.replace("-","_"), ["condition", "num_utterances", "total_duration_s"]]
+    speaker_row = df_speakers.loc[df_speakers["spk_id"] == label["speaker"], ["gender","age","num_sessions","total_utterances"]]
+
     # Extract features from config
-    speaker_id = id[:3]
-    session_id = id[3:7]
-    block_id = session_id[0]
-    prompt_id = id[7:10]
-    version = int(audio_file[underscore_idx+3:underscore_idx+5])
-    class_label = label[6]["value"]
-    sex = label[7]["value"]
-    age = label[8]["value"]
-    alcohol_habit = label[10]["value"]
-    br_ac = label[11]["value"]
-    bl_ac = label[12]["value"]
-    is_control_group = block_id in ["5","6"]
-    car_id = "CAR_B" if block_id in {"1","2","5"} else "CAR_A" # CAR_A = Passat | CAR_B = Open
-    sound_duration = float(sf.info(osp.join(WAV_H_DIR,audio_file)).duration)
+    speaker_id = label["speaker"]
+    session_id = label["session"]
+    bac_before = label["bac_before"]
+    bac_after = label["bac_after"]
+    class_label = label["label"]
+    condition = session_row["condition"].values[0]
+    num_utterances = session_row["num_utterances"].values[0]
+    total_duration_s = session_row["total_duration_s"].values[0]
+    sex = speaker_row["gender"].values[0]
+    age = speaker_row["age"].values[0]
+    num_sessions = speaker_row["num_sessions"].values[0]
+    total_utterances = speaker_row["total_utterances"].values[0]
+    sound_duration = float(sf.info(osp.join(WAV_DIR,wav_file)).duration)
 
-    # Cross validate with label config
-    assert label_config["annotates"] == audio_file
-    assert label_config["name"] == Path(audio_file).stem
-    assert label[0]["value"] == label_config["name"]
-    assert label[1]["value"] == id
-    assert label[2]["name"] == "spn"
-    assert label[4]["name"] == "item"
-    assert label[6]["name"] == "alc"
-    assert label[7]["name"] == "sex"
-    assert label[8]["name"] == "age"
-    assert label[10]["name"] == "drh"
-    assert label[11]["name"] == "aak"
-    assert label[12]["name"] == "bak"
-
-    # Validate dataset's README structure
-    if block_id in {"1","3"}:
-        assert label[6]["value"] == "a"
-    elif block_id in {"2","4"}:
-        assert label[6]["value"] == "na", f"error {block_id} | {label[6]["value"]}"
-    elif block_id in {"5","6"}:
-        assert label[6]["value"] == "cna", f"error {block_id} | {label[6]["value"]}"
-    else:
-        raise RuntimeError(f"Unknown block ID: {block_id}")
+    # Assertions
     
-    if class_label == "a":
-        assert block_id in {"1","3"}
-    elif class_label == "na":
-        assert block_id in {"2","4"}
-    elif class_label == "cna":
-        assert block_id in {"5","6"}
-    else:
-        raise RuntimeError(f"Unknown block ID: {block_id}")
-
-
     # Store data sample instance
     data_dict[id] = DataObject(
         id=id,
-        wav_file=audio_file,
-        label_file=label_file,
+        wav_file=wav_file,
         speaker_id=speaker_id,
         session_id=session_id,
-        prompt_id=prompt_id,
-        block_id=block_id,
-        version=version,
-        class_label=class_label,
-        sex=sex,
         age=age,
-        alcohol_habit=alcohol_habit,
-        br_ac=br_ac,
-        bl_ac=bl_ac,
-        is_control_group=is_control_group,
-        car_id=car_id,
-        sound_duration=sound_duration
+        sex=sex,
+        class_label=class_label,
+        condition=condition,
+        num_sessions=num_sessions,
+        session_utterances=num_utterances,
+        total_utterances=total_utterances,
+        sound_duration=sound_duration,
+        bac_before=bac_before,
+        bac_after=bac_after,
+        bac_delta=(bac_before-bac_after)
     )
 
 
 data_df = pd.DataFrame([asdict(data_obj) for data_obj in data_dict.values()])
-data_df.to_csv(osp.join(ROOT_DIR,"alc_data_samples.csv"), index=False)
-print(data_df.head())
+print(data_df.head(15))
+
+if not dry_run:
+    data_df.to_csv(osp.join(ROOT_DIR,"dac218_samples.csv"), index=False)
--- scripts/data_moving_dac218.py
+++ scripts/data_moving_dac218.py
@@ -1,19 +1,34 @@
 
 import os
+import re
+import json
 import time
 import shutil
 import os.path as osp
+import pandas as pd
 
 # Directories
-ROOT_DIR = osp.join("..","data","ALC")
+ROOT_DIR = osp.join("..","data","SE-DAC218")
+METADATA_DIR = osp.join(ROOT_DIR,"metadata")
+
+# read in csv files
+df_sessions = pd.read_csv(osp.join(METADATA_DIR,"sessions.csv"))
+df_speakers = pd.read_csv(osp.join(METADATA_DIR,"speakers.csv"))
+df_utterances = pd.read_csv(osp.join(METADATA_DIR,"utterances.csv"))
 
 dry_run = False # False means does actual copying
 is_verbose = False
 idx = 1
+delta_limit = 0.1
+max_delta = 0.0
+
+label_target_path = osp.join(ROOT_DIR,"labels","labels.json")
+label_cache = set()
+labels_list = []
 
 for (root,dirs,files) in os.walk(ROOT_DIR,topdown=True):
 
-    if "metadata" in root:
+    if "metadata" in root or "wav" in root:
         continue
 
     if is_verbose:
@@ -26,15 +41,35 @@ for (root,dirs,files) in os.walk(ROOT_DIR,topdown=True):
 
         source_path = osp.join(root,file)
 
-        if not file.endswith((".par",".TextGrid",".wav",".json")): continue
-        audio_channel_path = "h" if ("_h_" in file) else "m"
+        if not file.endswith(".wav"): continue
 
         if file.endswith(".wav"): # audio file
-            target_path = osp.join(ROOT_DIR,"wav",audio_channel_path,file)
-        elif file.endswith(".json"): # label file
-            target_path = osp.join(ROOT_DIR,"labels",audio_channel_path,file)
-        elif file.endswith((".par","TextGrid")):
-            target_path = osp.join(ROOT_DIR,"labels","metadata",audio_channel_path,file)
+
+            # Parse audio file
+            split_path = root.split("\\")
+            speaker = [x for x in split_path if x.startswith("spk")]
+            session = [x for x in split_path if x.startswith("sess")]
+            assert len(speaker) == 1, f"Expected only a single match both got {speaker}"
+            assert len(session) == 1, f"Expected only a single match both got {session}"
+            file_cleaned = file.replace("_bpillar","")
+            modified_file = speaker[0]+"-"+session[0]+"-"+file_cleaned
+            target_path = osp.join(ROOT_DIR,"audio","wav",modified_file)
+
+            # Label Construct label file
+            data_id = f"{speaker[0]}_{session[0]}" # all utterances in this session share same condition & BAC
+            if data_id not in label_cache:
+                label = {}
+                label["speaker"] = speaker[0]
+                label["session"] = session[0]
+                label_data = df_sessions.loc[df_sessions["session_id"] == data_id, ["condition", "bac_before", "bac_after"]].values[0]
+                label["label"] = "na" if (label_data[0] == "A") else "a"
+                label["bac_before"] = label_data[1]
+                label["bac_after"] = label_data[2]
+                if (delta := (label_data[1] - label_data[2])) > delta_limit:
+                    max_delta = max(max_delta, delta)
+                label_cache.add(data_id)
+                labels_list.append({"id": data_id.replace("_","-"), "label": label})
+
         else:
             raise RuntimeError(f"Unknown file type encounterd: {file}")
     
@@ -48,3 +83,10 @@ for (root,dirs,files) in os.walk(ROOT_DIR,topdown=True):
                 print(f"File {target_path} already exists")
 
         idx += 1
+
+print("max delta in data:",max_delta)
+
+# Finally write the labels file
+if not dry_run: 
+    with open(label_target_path, "w") as json_file:
+        json.dump(labels_list, json_file, indent=4)
